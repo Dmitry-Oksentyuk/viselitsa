300d950ce3a845366824932403502dd4db7e1e87
Dmitry-Oksentyuk <reklama215@gmail.com>
2016-12-06 20:28:57 +0300

    Update word_reader.rb

--- lib/word_reader.rb
+++ lib/word_reader.rb
@@ -9,7 +9,7 @@ class WordReader
   end
 
   def read_from_file(file_name)
-    nil unless File.exist?(file_name)
+    return nil unless File.exist?(file_name)
 
     f = File.new(file_name, "r:UTF-8")
     lines = f.readlines
